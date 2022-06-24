aebed08010b6da7ffce812c3de32d25dc179488a
michael86 <michael8t6@gmail.com>
2022-06-24 22:20:07 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// const mensNav = document.getElementById('mens-nav');
+// const womensNav = document.getElementById('womens-nav');
+
+// womensNav.addEventListener('click', e => {
+//     mensNav.style.gridColumn = "1 / span 1";
+//     womensNav.style.gridColumn = "2 / span 2";
+// })
+
+// mensNav.addEventListener('click', e => {
+//     womensNav.style.gridColumn = "3";
+//     mensNav.style.gridColumn = "1 / span 2";
+// })
