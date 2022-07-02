46adac78c2ea4b7c1156c4f7c8885b0869f4df08
michael86 <michael8t6@gmail.com>
2022-07-02 14:55:56 +0100

    completed mobile layout: see description
    Completed mobile layout, most of the design is similar, just need to adjust margins/paddings as well as make navbar transition to black background on scroll.
    
    Also need to make the location nav slide in and out.
    
    Lastly make responsive to desktop and refactor sass

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ const closeNav = document.getElementById('close-nav');
 navToggle.addEventListener('click', () => nav.classList.add('show-nav'));
 closeNav.addEventListener('click', () => nav.classList.remove('show-nav'));
 
+
+window.onscroll = function () {
+    "use strict";
+    if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
+        nav.classList.add("scroll");
+    } else {
+        nav.classList.remove("scroll");
+    }
+};
+
